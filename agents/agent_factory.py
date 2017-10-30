3a53f701c5cef04c328222f5f7a011cd3a6102f5
Bogusz <stefanczykb@gmail.com>
2017-10-30 18:55:35 +0100

    removed import neural network agent without need - speed up without importing tensor flow

--- agents/agent_factory.py
+++ agents/agent_factory.py
@@ -1,10 +1,12 @@
 from agents.gamestate_repo import GamestateRepo
 from config import MODEL_PATH
 
+def _createNNAgent(possible_keys, repo):
+    from agents.neural_network_agent import NeuralNetworkAgent
+    return NeuralNetworkAgent(MODEL_PATH, possible_keys, repo)
 
 def agent_factory(agent_name, possible_keys, save_path):
     from agents.random_agent import RandomAgent
-    from agents.neural_network_agent import NeuralNetworkAgent
 
     repo = None
     if isinstance(save_path, str):
@@ -12,5 +14,5 @@ def agent_factory(agent_name, possible_keys, save_path):
 
     return {
         'AGENT_RANDOM': lambda: RandomAgent(possible_keys, repo),
-        'AGENT_NN' : lambda: NeuralNetworkAgent(MODEL_PATH, possible_keys, repo)
+        'AGENT_NN' : lambda: _createNNAgent(possible_keys, repo)
     }[agent_name]()
