db067825b37d3aa4da91ebecb14456edffd8a4b5
Bogusz <stefanczykb@gmail.com>
2017-10-30 20:15:31 +0100

    fixed random agent not accepting time

--- agents/random_agent.py
+++ agents/random_agent.py
@@ -6,11 +6,11 @@ class RandomAgent:
         self.possible_keys = possible_keys
         self.repo = repo
 
-    def react_to_new_game_screen(self, screen_shot, score):
+    def react_to_new_game_screen(self, screen_shot, score, time):
         inputs = random.choice(self.possible_keys)
         screen = resize_128(screen_shot)
         if not self.repo is None:
-            self.repo.commit(screen, score, inputs)
+            self.repo.commit(screen, score, inputs, time)
 
         return inputs
 
