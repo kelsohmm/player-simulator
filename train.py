6ae88c5b73a2eb527ff1addc0e9d0efc670666c8
Bogusz <stefanczykb@gmail.com>
2017-11-07 18:59:42 +0100

    added second convolutional layer, enlarged hidden layers

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import numpy as np
-from config import DATA_SAVE_PATH, MODEL_SAVE_PATH, MODEL_PREVIEW_PATH
 import keras as K
+from config import DATA_SAVE_PATH, MODEL_SAVE_PATH, MODEL_PREVIEW_PATH
 
 loaded = np.load(DATA_SAVE_PATH)
 inputs_keys = loaded['inputs_keys']
@@ -14,18 +14,20 @@ time_input = K.Input(shape=(1,))
 keys_input = K.Input(shape=(3,))
 frame_input = K.Input(shape=(128, 128, 3))
 
-conv = K.layers.Conv2D(filters=64, kernel_size=(4, 4), input_shape=(128, 128, 3), data_format='channels_last')
-conved_frame = K.layers.Flatten()(conv(frame_input))
+conv = K.layers.Conv2D(filters=32, kernel_size=6, strides=2, name='conv_1', input_shape=(128, 128, 3), data_format='channels_last')(frame_input)
+conv = K.layers.Conv2D(filters=64, kernel_size=3, strides=2, name='conv_2')(conv)
+conved_frame = K.layers.Flatten()(conv)
+# flatted_frame = K.layers.Flatten()(frame_input)
 
-hidden = K.layers.concatenate([time_input, keys_input, conved_frame])
-hidden = K.layers.Dense(64, activation='relu')(hidden)
-hidden = K.layers.Dense(32, activation='relu')(hidden)
+concat = K.layers.concatenate([time_input, keys_input, conved_frame], name='concat')
+hidden = K.layers.Dense(512, activation='relu', name='hidden_1')(concat)
+hidden = K.layers.Dense(256, activation='relu', name='hidden_2')(hidden)
 main_output = K.layers.Dense(1, name='main_output')(hidden)
 
 model = K.models.Model(inputs=[time_input, keys_input, frame_input],
-                           outputs=[main_output])
+                       outputs=[main_output])
 
-model.compile(optimizer='rmsprop', loss='mean_squared_error')
+model.compile(optimizer='rmsprop', loss=K.losses.mean_squared_error)
 
 history = model.fit([inputs_time, inputs_keys, inputs_frame],
                       [labels],
