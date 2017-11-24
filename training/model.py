2f614845d25639f280e5c3f4ec573aa6011adaa1
Bogusz <stefanczykb@gmail.com>
2017-11-24 19:11:45 +0100

    removed nan-based loss function, use batches with same action_idx instead

--- training/model.py
+++ training/model.py
@@ -1,11 +1,5 @@
-import tensorflow as tf
 import keras as K
 from config import MODEL_PREVIEW_PATH, CONV_SHAPE
-
-
-def loss_mse_for_known(y_true, y_pred):
-    replaced = tf.where(tf.is_nan(y_true), y_pred, y_true)
-    return K.backend.mean(K.backend.square(replaced - y_pred), axis=-1)
 
 
 def create_network():
@@ -20,19 +14,17 @@ def create_network():
     flat_frame = K.layers.Flatten()(frame_input)
 
     concat = K.layers.concatenate([conved_frame, flat_frame])
-    hidden = K.layers.Dense(2048, activation='relu', name='hidden_1')(concat)
+    hidden = K.layers.Dense(512, activation='relu', name='hidden_1')(concat)
     hidden = K.layers.Dense(512, activation='relu', name='hidden_2')(hidden)
 
     output_names = ["output_%d" % i for i in range(6)]
-    output_layers = [K.layers.Dense(1, name=name)(
-                        K.layers.Dense(128)(hidden)
-                     )
+    output_layers = [K.layers.Dense(1, name=name)(hidden)
                      for name in output_names]
 
     model = K.models.Model(inputs=[frame_input],
                            outputs=output_layers)
 
-    model.compile(optimizer=K.optimizers.Adam(), loss={name: loss_mse_for_known for name in output_names})
+    model.compile(optimizer=K.optimizers.Adam(), loss={name: K.losses.mean_squared_error for name in output_names})
 
     try:
         K.utils.plot_model(model, show_shapes=True, to_file=MODEL_PREVIEW_PATH)
--- training/train.py
+++ training/train.py
@@ -14,7 +14,9 @@ class ModelTraining:
         if self.repo.size() > LEARNING_START:
             memories = self.repo.get_commits_batch_with_last(BATCH_SIZE)
             samples, labels = self._map_memories_to_train_data(memories)
-            loss = self.model.train_on_batch(samples, np.split(labels, self.no_outputs, axis=1))
+            loss = self.model.train_on_batch(x=samples,
+                                             y=np.split(labels, self.no_outputs, axis=1),
+                                             class_weight=self._activaty_only_trained_action_idx(memories))
             return loss
         else:
             return 0.
@@ -23,7 +25,6 @@ class ModelTraining:
         no_memories = len(memories)
         samples = np.zeros((no_memories,) + CONV_SHAPE, dtype=np.ubyte)
         labels = np.zeros((no_memories, self.no_outputs))
-        labels[:, :] = np.nan
         for idx in range(no_memories):
             prev_screen, action_idx, reward, next_screen = memories[idx]
             samples[idx] = prev_screen
@@ -36,3 +37,24 @@ class ModelTraining:
             return predictions.max()
         else:
             return 0.0
+
+    def _equal_actions_ids(self, memories):
+        _, first_action_idx, _, _ = memories[0]
+        for i in range(1, len(memories)):
+            _, action_idx, _, _ = memories[i]
+            if first_action_idx != action_idx:
+                return False
+        return True
+
+    def _trained_action_idx(self, memories):
+        assert self._equal_actions_ids(memories), "All action ids must be equal"
+        _, first_action_idx, _, _ = memories[0]
+        return first_action_idx
+
+    def _activaty_only_trained_action_idx(self, memories):
+        trained_action_idx = self._trained_action_idx(memories)
+        class_weights = [0.0] * self.no_outputs
+        class_weights[trained_action_idx] = 1.0
+        return class_weights
+
+
