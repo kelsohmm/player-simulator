22b96e0e7c5f3735d46ad82c344a1d75840e4c38
Bogusz <stefanczykb@gmail.com>
2017-11-24 18:06:51 +0100

    removed random_agent to lower complexity

--- agents/agent_factory.py
+++ agents/agent_factory.py
@@ -5,11 +5,9 @@ def _createNNAgent(possible_keys, repo, model):
     return NeuralNetworkAgent(model, possible_keys, repo)
 
 def agent_factory(agent_name, possible_keys, save_path, model):
-    from agents.random_agent import RandomAgent
 
     repo = Repo(save_path)
 
     return {
-        'AGENT_RANDOM': lambda: RandomAgent(possible_keys, repo),
         'AGENT_NN' : lambda: _createNNAgent(possible_keys, repo, model)
     }[agent_name]()
