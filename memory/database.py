74871a82793e4fabf890914a02d66bfb9e7395cc
Bogusz <stefanczykb@gmail.com>
2017-12-14 20:36:56 +0100

    added timestamp to database transitions

--- memory/database.py
+++ memory/database.py
@@ -1,16 +1,18 @@
-import random
 import sqlite3
 from sqlite3 import Binary
 import os
 
-_DATABASE_SCHEMA = '''CREATE TABLE transitions
-                                 (game_id INT,
-                                  time INT,
-                                  curr_state BLOB NOT NULL, 
-                                  action_idx INT NOT NULL,
-                                  reward REAL NOT NULL, 
-                                  next_state BLOB,
-                                  PRIMARY KEY (game_id, time))'''
+# TODO: change reward to absolute score
+_DATABASE_SCHEMA = '''
+    CREATE TABLE transitions (
+        game_id INT NOT NULL,
+        frame_number INT NOT NULL,
+        curr_state BLOB NOT NULL,
+        action_idx INT NOT NULL,
+        reward REAL NOT NULL,
+        next_state BLOB,
+        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP); 
+'''
 
 class Database:
     def __init__(self, filepath):
@@ -27,12 +29,12 @@ class Database:
     def fetch_random_batch(self, batch_size, action_idx):
         return list(self.cursor.execute('SELECT * FROM transitions WHERE action_idx=%d ORDER BY RANDOM() LIMIT %d' % (batch_size, action_idx)))
 
-    def insert_transition(self, game_id, time, curr_state, action_idx, reward, next_state):
+    def insert_transition(self, game_id, frame_number, curr_state, action_idx, reward, next_state):
         self.db_size += 1
         curr_state = Binary(curr_state)
         next_state = Binary(next_state) if next_state is not None else None
-        self.cursor.execute('INSERT INTO transitions VALUES (?,?,?,?,?,?)',
-                            (game_id, time, curr_state, action_idx, reward, next_state))
+        self.cursor.execute('INSERT INTO transitions (game_id, frame_number, curr_state, action_idx, reward, next_state) ' # do not insert timestamp
+                            'VALUES (?,?,?,?,?,?)', (game_id, frame_number, curr_state, action_idx, reward, next_state))
         self.conn.commit()
 
     def _query_transitions_count(self):
